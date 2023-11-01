1881d997c182626ed212c8bdd949192225dfd8b2
howooking <junsgk@gmail.com>
2023-11-01 11:53:50 +0900

    footer responsive

--- components/sections/footer/footer.tsx
+++ components/sections/footer/footer.tsx
@@ -2,19 +2,28 @@ import Container from "@/components/container";
 
 export default function Footer() {
   return (
-    <div className=" bg-stone-900  py-10 text-sm text-stone-100">
+    <div className="bg-stone-800 py-5 text-xs text-stone-100 sm:py-10 sm:text-sm">
       <Container>
-        <div className="flex items-center justify-between">
+        <div className="flex justify-between">
           <div>
             <p>리베동물메디컬센터</p>
-            <p>서울특별시 송파구 삼전로 56 리베동물메디컬센터</p>
+            <p>
+              서울특별시 송파구 삼전로 56 <br className="md:hidden" />
+              리베동물메디컬센터
+            </p>
             <p>liebeamc2@daum.net</p>
-            <p>Copyright (c) 2014 리베동물메디컬센터 All Rights Reserved.</p>
+            <p>
+              Copyright (c) 2014 <br className="md:hidden" /> 리베동물메디컬센터
+              <br className="md:hidden" />
+              All Rights Reserved.
+            </p>
           </div>
-          <div className="text-end">
-            <p className="text-2xl text-primary">02.6953.7502</p>
-            <p>사업자 등록번호 : 530-36-01377</p>
-            <p>대표 : 김홍석</p>
+          <div className="flex flex-col justify-between text-end">
+            <p className="text-xl text-primary sm:text-2xl">02.6953.7502</p>
+            <div>
+              <p>사업자등록번호 5303601377</p>
+              <p>대표 김홍석</p>
+            </div>
           </div>
         </div>
       </Container>
