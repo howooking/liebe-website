f3df97a634e757498618c34553152d707f0cee74
howooking <junsgk@gmail.com>
2023-11-13 23:56:57 +0900

    히어로 위계

--- components/sections/hero/copy.tsx
+++ components/sections/hero/copy.tsx
@@ -9,7 +9,7 @@ export default function Copy({ subtitle, title }: CopyProps) {
   return (
     <div className="absolute left-0 top-0 z-50 w-screen">
       <div
-        className="flex h-screen w-full flex-col items-center justify-center "
+        className="flex h-[100dvh] w-full flex-col items-center justify-center "
         // initial={{ opacity: 0, y: -40 }}
         // whileInView={{ opacity: 1, y: 0 }}
         // transition={{
@@ -18,10 +18,9 @@ export default function Copy({ subtitle, title }: CopyProps) {
         // }}
         // viewport={{ once: false }}
       >
-        <div className="relative flex flex-col items-center text-white">
+        <div className="relative flex flex-col items-center gap-2 text-white sm:gap-4 lg:gap-6">
           <p className="text-xl font-bold sm:text-3xl lg:text-5xl">{title}</p>
-          <p className="text-sm sm:text-xl lg:text-2xl">{subtitle}</p>
-          <div className="absolute inset-0 -z-10 bg-black/50 blur-3xl" />
+          <p className="text-sm sm:text-xl lg:text-3xl">{subtitle}</p>
         </div>
       </div>
     </div>
