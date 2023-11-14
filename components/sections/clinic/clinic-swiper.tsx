96a48c038ac04ff4a3da816bcb1b9f353a70be7c
howooking <junsgk@gmail.com>
2023-11-14 15:26:01 +0900

    클리닉 스와이퍼 애니메이션 delay안보이는 부분은 0으로 수정

--- components/sections/clinic/clinic-swiper.tsx
+++ components/sections/clinic/clinic-swiper.tsx
@@ -37,6 +37,34 @@ export default function ClinicSwiper() {
       return 3.2;
     }
   }, [width]);
+
+  const delay = useCallback(
+    (index: number) => {
+      if (!width) {
+        return 0.5;
+      }
+      // sm
+      if (width <= 640) {
+        return 0.5;
+      }
+      // md
+      if (width > 640 && width <= 768) {
+        if (index <= 2) {
+          return 0.5 * index;
+        }
+        return 0;
+      }
+      // lg
+      if (width > 768) {
+        if (index <= 3) {
+          return 0.5 * index;
+        }
+        return 0;
+      }
+    },
+    [width],
+  );
+
   const spaceBetween = useCallback(() => {
     if (!width) {
       return 24;
@@ -63,8 +91,8 @@ export default function ClinicSwiper() {
           <motion.div
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5 * index }}
-            viewport={{ once: false }}
+            transition={{ duration: 0.5, delay: delay(index) }}
+            viewport={{ once: true }}
           >
             <Image
               src={clinic.imgSrc}
