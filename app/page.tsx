d238c5a527bbce52fc60046b356a84d3c988b0d2
howooking <junsgk@gmail.com>
2023-11-14 20:06:58 +0900

    비디오

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <ClinicSection />
       <TestimonialSection />
       <MapSection />
-      {/* <VideosSection /> */}
+      <VideosSection />
     </>
   );
 }
--- components/sections/videoes/videos-section.tsx
+++ components/sections/videoes/videos-section.tsx
@@ -1,17 +1,13 @@
 import Section from "@/components/common/section";
 import Video from "./video";
 import Container from "@/components/common/container";
-import Title from "@/components/common/title";
 
 export default function VideosSection() {
   return (
     <Section id="video">
-      <div className="bg-slate-100 py-8">
+      <div className="bg-stone-100 py-12">
         <Container>
-          <div className="mb-8 text-center">
-            <Title>리베 유튜브 동영상</Title>
-          </div>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-3">
             {VIDEOS.map((video) => (
               <Video src={video} key={video} />
             ))}
