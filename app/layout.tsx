9ca340dd93972f6a5fafbc65a68de7c6d14bc128
howooking <junsgk@gmail.com>
2023-11-21 23:47:04 +0900

    SEO 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import "@/app/globals.css";
-
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 
+import "@/app/globals.css";
 import Navbar from "@/components/common/navbar/navbar";
 import FooterSection from "@/components/sections/footer/footer-section";
 import FloatingSocials from "@/components/common/floating-socials";
@@ -16,8 +15,7 @@ const font = Noto_Sans_KR({ subsets: ["latin"] });
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: "리베동물메디컬센터",
-  description:
-    "잠실 삼전동 24시동물병원 동물병원 리베동물병원 리베동물메디컬센터",
+  description: "서울특별시 잠실구 삼전동 24시동물병원",
   keywords: [
     "리베동물병원",
     "동물병원",
@@ -26,21 +24,16 @@ export const metadata: Metadata = {
     "삼전동동물병원",
     "24시동물병원",
     "강아지암",
-    "강아지암센터",
     "고양이암",
-    "고양이내과",
-    "고양이병원",
     "고양이동물병원",
     "고양이전문병원",
     "강아지건강검진",
     "고양이건강검진",
     "강아지mri",
     "고양이mri",
-    "동물mri",
     "동물병원mri",
     "강아지ct",
     "고양이ct",
-    "동물ct",
     "동물병원ct",
     "특수동물",
     "특수동물진료",
@@ -50,35 +43,23 @@ export const metadata: Metadata = {
     "토끼진료",
     "앵무새진료",
     "슈가글라이더진료",
-    "라쿤진료 강아지심장병",
+    "라쿤진료",
+    "강아지심장병",
     "고양이심장병",
     "강아지신부전",
-    "강아지신장",
-    "강아지쿠싱증후군",
     "고양이신부전",
-    "강아지투석",
-    "줄기세포동물병원",
-    "강아지줄기세포",
-    "고양이줄기세포",
+    "강아지쿠싱증후군",
     "강아지마취",
     "고양이마취",
     "강아지치과",
     "강아지스케일링",
-    "동물치과",
-    "동물치과병원",
     "강아지치석제거",
     "고양이스케일링",
     "고양이구내염",
     "고양이치과",
-    "고양이치과병원",
     "강아지안과",
-    "강아지안과병원",
-    "강아지안과전문병원",
-    "안과전문동물병원",
     "강아지녹내장",
-    "강아지백내장",
     "고양이안과",
-    "고양이백내장",
     "고양이녹내장",
     "강아지수혈",
     "고양이수혈",
@@ -87,10 +68,12 @@ export const metadata: Metadata = {
     "강아지침치료",
     "동물한방병원",
     "한방동물병원",
-    "강아지재활",
     "강아지재활센터",
-    "강아지수중재활",
   ],
+  applicationName: "리베동물병원 홈페이지",
+  category: "동물병원",
+  classification: "동물병원",
+  creator: " Howoo",
 };
 
 export default function RootLayout({
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,25 +13,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${baseUrl}/info/time`,
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 1,
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/info/visit`,
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 1,
+      priority: 0.5,
     },
     {
       url: `${baseUrl}/staff`,
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 1,
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/facility`,
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 1,
+      priority: 0.7,
     },
   ];
 }
