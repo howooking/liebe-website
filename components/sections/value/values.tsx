133a3166bb6721a8d6652f5b7cd50962ee2a3625
howooking <junsgk@gmail.com>
2023-11-22 10:45:45 +0900

    value 섹션 리팩토링

---
+++ components/sections/value/values.tsx
@@ -0,0 +1,49 @@
+import Highlight from "@/components/common/highlight";
+import { FaHeart, FaHandshakeSimple, FaRoad } from "react-icons/fa6";
+
+export const VALUES = [
+  {
+    title: (
+      <>
+        고객과의 <Highlight>신뢰</Highlight>를 우선으로 하는
+      </>
+    ),
+    subtitle: (
+      <>
+        최고 수준의 전문 의료진과 정성스러운 친절 서비스로, 보호자님에게{" "}
+        <Highlight className="font-semibold">신뢰</Highlight>를 안겨드립니다.
+      </>
+    ),
+    Icon: FaHandshakeSimple,
+  },
+  {
+    title: (
+      <>
+        환자를 내자식과 같이 <Highlight>사랑</Highlight>하는
+      </>
+    ),
+    subtitle: (
+      <>
+        <Highlight className="font-semibold">사랑</Highlight>과 전문 의료로
+        당신의 반려동물을 책임지고 지켜드립니다. 함께하는 따뜻한 치료, 언제나
+        당신과 함께합니다.
+      </>
+    ),
+    Icon: FaHeart,
+  },
+  {
+    title: (
+      <>
+        <Highlight>원칙</Highlight>과 타협하지 않는
+      </>
+    ),
+    subtitle: (
+      <>
+        <Highlight className="font-semibold">원칙</Highlight>을 바탕으로 하는
+        전문 의료로, 당신의 반려동물을 안전하게 돌봐 드립니다. 높은 기술과
+        인간적인 손길로 편안한 치료를 약속합니다.
+      </>
+    ),
+    Icon: FaRoad,
+  },
+];
