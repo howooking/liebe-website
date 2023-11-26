220847b27ff50fd4db8ad7bca2ec129d812f0b94
howooking <junsgk@gmail.com>
2023-11-26 20:31:22 +0900

    robots.ts

--- app/robot.ts
+++ app/robot.ts
@@ -4,7 +4,8 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      allow: "*",
+      userAgent: "*",
+      allow: "/",
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
